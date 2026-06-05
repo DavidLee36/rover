c90a579e9791691efb74f856bbebfcb22f248541
DavidLee36 <david303lee@gmail.com>
2026-06-04 18:32:01 -0700

    only limit right when going forward

--- Code/pi/communicate.py
+++ Code/pi/communicate.py
@@ -16,4 +16,5 @@ def send(msg):
 def axis_motion_to_teensy(arr):
 	left_dir = "F" if arr[1] > 0 else "R"
 	right_dir = "F" if arr[3] > 0 else "R"
+	r_multi = config.curr_right_multiplier if arr[1] > 0 and arr[3] > 0 else 1
 	return f"{left_dir}{round(abs(arr[1]) * config.curr_max_speed)}|{right_dir}{round(abs(arr[3]) * config.curr_max_speed * config.curr_right_multiplier)}"
