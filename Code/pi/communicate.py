1624e9e01cc22ae1fe94b3647c4489f779e18e2d
DavidLee36 <david303lee@gmail.com>
2026-06-04 18:44:30 -0700

    not bool :(

--- Code/pi/communicate.py
+++ Code/pi/communicate.py
@@ -16,5 +16,5 @@ def send(msg):
 def axis_motion_to_teensy(arr):
 	left_dir = "F" if arr[1] > 0 else "R"
 	right_dir = "F" if arr[3] > 0 else "R"
-	r_multi = config.curr_right_multiplier if left_dir and right_dir else 1
+	r_multi = config.curr_right_multiplier if left_dir == "F" and right_dir == "F" else 1
 	return f"{left_dir}{round(abs(arr[1]) * config.curr_max_speed)}|{right_dir}{round(abs(arr[3]) * config.curr_max_speed * r_multi)}"
